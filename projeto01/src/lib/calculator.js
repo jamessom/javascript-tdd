f747b692e79546adc7487a43cff25bac64e8774c
jamessom <jamessomqueiroz@gmail.com>
2021-04-18 23:54:29 -0300

    feat: adiciona métodos e testes de uma calculadora simples de adição e subtração

---
+++ projeto01/src/lib/calculator.js
@@ -0,0 +1,21 @@
+module.exports.sum = (paramOne, paramTwo) => {
+  const firstNumber = parseInt(paramOne, 10);
+  const secondNumber = parseInt(paramTwo, 10);
+
+  if (Number.isNaN(firstNumber) || Number.isNaN(secondNumber)) {
+    throw Error('Params should be number.');
+  }
+
+  return firstNumber + secondNumber;
+};
+
+module.exports.subtract = (paramOne, paramTwo) => {
+  const firstNumber = parseInt(paramOne);
+  const secondNumber = parseInt(paramTwo);
+
+  if(Number.isNaN(firstNumber) || Number.isNaN(secondNumber)) {
+    throw Error('Params must be a number.');
+  }
+
+  return firstNumber - secondNumber;
+};
---
+++ projeto01/src/lib/calculator.spec.js
@@ -0,0 +1,39 @@
+const { sum, subtract } = require('./calculator');
+
+describe('when sum is called', () => {
+  it('should sum 2 and 2 and result must be 4', () => {
+    expect(sum(2,2)).toBe(4);
+    expect(sum(2,2)).not.toBe(22);
+  });
+
+  it('should sum 2 and 2 and even if one of then is a string the result must be 4', () => {
+    expect(sum('2', 2)).toBe(4);
+    expect(sum('2', 2)).not.toBe(22);
+  });
+
+  it('should throw an error if what is provided to the method cannot be summed', () => {
+    expect(() => sum('', 2)).toThrowError();
+    expect(() => sum([2,2])).toThrowError();
+    expect(() => sum({})).toThrowError();
+    expect(() => sum()).toThrowError();
+  });
+});
+
+describe('when subtraction is called', () => {
+  it('should 5 subtract 3 and the result must be 2', () => {
+    expect(subtract(5, 3)).toBe(2);
+    expect(subtract(5, 3)).not.toBe(4);
+  });
+
+  it('should 5 subtract 3 and even if one then is a string the result must be 2', () => {
+    expect(subtract('5', 3)).toBe(2);
+    expect(subtract('5', 3)).not.toBe(4);
+  });
+
+  it('should throw an error if what is provided to the method cannot be subtracted', () => {
+    expect(() => subtract('', 2)).toThrowError();
+    expect(() => subtract([2, 2])).toThrowError();
+    expect(() => subtract({})).toThrowError();
+    expect(() => subtract()).toThrowError();
+  });
+});
