f746ae267d56056212bc6dce228aea0e3672a917
jamessom <jamessomqueiroz@gmail.com>
2021-04-25 17:08:27 -0300

    feat: adiciona validação se é um objeto e não um array

--- projeto01/src/lib/queryString.js
+++ projeto01/src/lib/queryString.js
@@ -1,4 +1,10 @@
 module.exports.queryString = object =>
   Object.entries(object)
-    .map(([key, value]) => `${key}=${value}`)
+    .map(([key, value]) => {
+      if (typeof value === 'object' && !Array.isArray(value)) {
+        throw Error('Please check your params.');
+      }
+
+      return `${key}=${value}`;
+    })
     .join('&');
--- projeto01/src/lib/queryString.spec.js
+++ projeto01/src/lib/queryString.spec.js
@@ -8,7 +8,43 @@ describe('Object to queryString', () => {
     };
 
     expect(queryString(testObject)).toBe('name=Jamessom&surname=Queiroz');
-
     expect(queryString(testObject)).not.toBe('name:Jamessom&surname:Queiroz');
+  });
+
+  it('should create a valid query string even when an arrays is passed as value', () => {
+    const testObject = {
+      name: 'Jamessom',
+      surname: 'Queiroz',
+      families: ['Queiroz', 'Chaves', 'Ribeiro'],
+    };
+
+    expect(queryString(testObject)).toBe(
+      'name=Jamessom&surname=Queiroz&families=Queiroz,Chaves,Ribeiro',
+    );
+    expect(queryString(testObject)).not.toBe('name:Jamessom&surname:Queiroz');
+  });
+
+  it('should throw an error when an object is passed as a value', () => {
+    const testObject = {
+      name: 'Jamessom',
+      surname: 'Queiroz',
+      families: {
+        first: 'Queiroz',
+        second: 'Chaves',
+        third: 'Ribeiro',
+      },
+    };
+
+    expect(() => {
+      queryString(testObject);
+    }).toThrowError();
+
+    expect(() => {
+      queryString({
+        name: 'Jamessom',
+        surname: 'Queiroz',
+        families: [],
+      });
+    }).not.toThrowError();
   });
 });
