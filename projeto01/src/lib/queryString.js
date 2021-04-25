0ec096790253d32368c9121f7d162249559720fe
jamessom <jamessomqueiroz@gmail.com>
2021-04-25 17:10:46 -0300

    refactor: extrai corpo da função para um método

--- projeto01/src/lib/queryString.js
+++ projeto01/src/lib/queryString.js
@@ -1,10 +1,10 @@
-module.exports.queryString = object =>
-  Object.entries(object)
-    .map(([key, value]) => {
-      if (typeof value === 'object' && !Array.isArray(value)) {
-        throw Error('Please check your params.');
-      }
+const keyValueToString = ([key, value]) => {
+  if (typeof value === 'object' && !Array.isArray(value)) {
+    throw Error('Please check your params.');
+  }
 
-      return `${key}=${value}`;
-    })
-    .join('&');
+  return `${key}=${value}`;
+};
+
+module.exports.queryString = object =>
+  Object.entries(object).map(keyValueToString).join('&');
